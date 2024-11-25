e24a2f7baf263aa94740aa56660cf2f6cc42d1e2
Gaelle Ozon <gozon@made-f0Br8s12.clusters.42paris.fr>
2024-11-25 13:19:08 +0100

    Included some libraries

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 13:16:02 by gozon             #+#    #+#             */
+/*   Updated: 2024/11/25 13:18:40 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
