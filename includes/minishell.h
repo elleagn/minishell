e1e54a537c494f40d5e5404111827022c5515fcb
Gaelle Ozon <gozon@made-f0Br8s12.clusters.42paris.fr>
2024-11-27 10:40:53 +0100

    Tokenizer pseudocode

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 13:16:02 by gozon             #+#    #+#             */
-/*   Updated: 2024/11/26 13:51:17 by gozon            ###   ########.fr       */
+/*   Updated: 2024/11/27 10:23:59 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,24 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <libft.h>
+
+typedef enum e_lexeme
+{
+	STRING_SIMPLE,
+	STRING_DOUBLE,
+	LESS,
+	LESSLESS,
+	GREATER,
+	GREATERGREATER,
+	PIPE,
+	WORD
+}	t_lexeme;
+
+typedef struct s_token
+{
+	t_lexeme	type;
+	char		*literal;
+	int			position;
+}	t_token;
 
 #endif
--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 15:29:41 by gozon             #+#    #+#             */
-/*   Updated: 2024/11/26 15:37:17 by gozon            ###   ########.fr       */
+/*   Updated: 2024/11/27 10:39:28 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,27 @@ int	count_words(char *str)
 		i++;
 	}
 	return (res);
+}
+
+t_list	*tokenizer(char *input)
+{
+	int		i;
+	t_list	*tokens;
+	int		error;
+
+	tokens = NULL;
+	i = 0;
+	error = 0;
+	while (input[i])
+	{
+		if (is_operator(input[i]))
+			error = add_operator(input, tokens, &i);
+		else if (is_string(input[i]))
+			error = add_string(input, tokens, &i);
+		else
+			error = add_word(input, tokens, &i);
+		if (error)
+			return (ft_lstclear(&tokens, clear_token), NULL);
+	}
+	return (tokens);
 }
