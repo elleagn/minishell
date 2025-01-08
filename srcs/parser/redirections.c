17600ed1a55f554219ea87131d3c7100917eb44c
lcluzan <lcluzan@student.42.fr>
2025-01-08 11:13:40 +0100

    Add heredoc (not working)

--- srcs/parser/redirections.c
+++ srcs/parser/redirections.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 15:20:47 by nouillebobb       #+#    #+#             */
-/*   Updated: 2025/01/08 16:55:58 by gozon            ###   ########.fr       */
+/*   Updated: 2025/01/08 11:12:27 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,8 @@ t_redir	*init_and_fill_redir(t_type type, t_token *filename)
 	if (!redir)
 		return (NULL);
 	redir->type = type;
-	// if (type == HERE_DOC)
-	// 	redir->filename = handle_here_doc(filename);
-	// else
-		redir->filename = ft_strdup(filename->literal);
+	// insert heredoc here
+	redir->filename = ft_strdup(filename->literal);
 	if (!redir->filename)
 		return (clear_redir(redir), NULL);
 	redir->backup = ft_strdup(filename->literal);
