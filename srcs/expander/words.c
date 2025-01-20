aa0a83e42b21c249bb8d180f67b0602051f2214a
Gaelle Ozon <gozon@paul-f4Ar7s8.clusters.42paris.fr>
2025-01-20 10:56:00 +0100

    Re-add words.c

---
+++ srcs/expander/words.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   words.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 11:49:50 by gozon             #+#    #+#             */
+/*   Updated: 2025/01/20 09:17:08 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+char	*join_var_name(char *expanded, char *var_name, t_data *data)
+{
+	char	*var;
+	int		itoa;
+
+	itoa = 0;
+	if (!ft_strncmp(var_name, "?", 2))
+	{
+		var = ft_itoa(data->exit_code);
+		itoa = 1;
+		if (!var)
+		{
+			data->exit_code = -1;
+			return (NULL);
+		}
+	}
+	else
+		var = mini_getenv(var_name, data->env);
+	if (!var)
+		return (expanded);
+	expanded = ft_strjoin(expanded, var);
+	if (itoa)
+		free(var);
+	if (!expanded)
+		data->exit_code = -1;
+	return (expanded);
+}
+
+char	*expand_var(char *word, t_data *data)
+{
+	char	*expanded;
+	char	**var_names;
+	int		i;
+
+	i = 0;
+	expanded = NULL;
+	var_names = ft_split(word, '$');
+	if (!var_names)
+	{
+		data->exit_code = -1;
+		return (NULL);
+	}
+	while (var_names[i])
+	{
+		replace_string(&expanded, join_var_name(expanded, var_names[i], data));
+		if (data->exit_code == -1)
+			return (free_char_array(var_names), NULL);
+		i++;
+	}
+	free_char_array(var_names);
+	return (expanded);
+}
+
+int	insert_word(char *literal, t_token *tlst)
+{
+	t_token	*token;
+
+	token = init_token();
+	if (!token)
+		return (1);
+	token->type = WORD;
+	token->literal = literal;
+	token->next = tlst->next;
+	token->prev = tlst;
+	tlst->next = token;
+	if (token->next)
+		token->next->prev = token;
+	token->position = tlst->position;
+	return (0);
+}
+
+void	split_words(t_token *token, t_data *data)
+{
+	char	**split;
+	int		i;
+
+	split = ft_split(token->literal, ' ');
+	if (!split)
+	{
+		data->exit_code = -1;
+		return ;
+	}
+	replace_string(&token->literal, split[0]);
+	i = 1;
+	while (split[i])
+	{
+		if (insert_word(split[i], token))
+		{
+			data->exit_code = -1;
+			break ;
+		}
+		i++;
+		token = token->next;
+	}
+	while (split[i++])
+		free(split[i]);
+	free(split);
+}
